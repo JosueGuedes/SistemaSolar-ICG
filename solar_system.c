d3c5b03a84376195cd48835ab6e8a6665b2fb129
Josue G <josuedev413@gmail.com>
2026-03-31 16:03:27 -0300

    Astros movimentando em elipses, adição de astros 3 a 6

--- solar_system.c
+++ solar_system.c
@@ -5,6 +5,134 @@
 // Variaveis para translação de cubos
 static float angulo = 0.0f;
 
+// Variaveis de tempo
+float t1 = 0.0f;
+float t2 = 0.0f;
+float t3=0, t4=0, t5=0, t6=0, t7=0, t8=0;
+
+
+typedef struct {
+    float x, y, z;
+} Vec3;
+
+Vec3 catmullRom(Vec3 p0, Vec3 p1, Vec3 p2, Vec3 p3, float t) {
+    float t2 = t * t;
+    float t3 = t2 * t;
+
+    Vec3 r;
+
+    r.x = 0.5f * ((2*p1.x) +
+        (-p0.x + p2.x)*t +
+        (2*p0.x - 5*p1.x + 4*p2.x - p3.x)*t2 +
+        (-p0.x + 3*p1.x - 3*p2.x + p3.x)*t3);
+
+    r.y = 0.5f * ((2*p1.y) +
+        (-p0.y + p2.y)*t +
+        (2*p0.y - 5*p1.y + 4*p2.y - p3.y)*t2 +
+        (-p0.y + 3*p1.y - 3*p2.y + p3.y)*t3);
+
+    r.z = 0.5f * ((2*p1.z) +
+        (-p0.z + p2.z)*t +
+        (2*p0.z - 5*p1.z + 4*p2.z - p3.z)*t2 +
+        (-p0.z + 3*p1.z - 3*p2.z + p3.z)*t3);
+
+    return r;
+}
+
+Vec3 getSplinePoint(Vec3* pts, int n, float t) {
+    float total = t * n;
+    int i = (int)total;
+    float localT = total - i;
+
+    int i0 = (i - 1 + n) % n;
+    int i1 = i % n;
+    int i2 = (i + 1) % n;
+    int i3 = (i + 2) % n;
+
+    return catmullRom(
+        pts[i0],
+        pts[i1],
+        pts[i2],
+        pts[i3],
+        localT
+    );
+}
+
+// ----------------- PONTOS DE CONTROLE DO MOVIMENTO DOS ASTROS --------------
+Vec3 orbitaMercurio[] = {
+    {6, 0, 0},
+    {3, 0, 2},
+    {0, 0, 3},
+    {-3, 0, 2},
+    {-6, 0, 0},
+    {-3, 0, -2},
+    {0, 0, -3},
+    {3, 0, -2}
+};
+
+int norbitaMercurio = 8;
+
+Vec3 orbitaVenus[] = {
+    {8, 0, 0},
+    {4, 0, 3},
+    {0, 0, 5},
+    {-4, 0, 3},
+    {-8, 0, 0},
+    {-4, 0, -3},
+    {0, 0, -5},
+    {4, 0, -3}
+};
+
+int norbitaVenus = 8;
+
+Vec3 orbitaTerra[] = {
+    {10, 0, 0},
+    {5, 0, 4},
+    {0, 0, 6},
+    {-5, 0, 4},
+    {-10, 0, 0},
+    {-5, 0, -4},
+    {0, 0, -6},
+    {5, 0, -4}
+};
+int nOrbitaTerra = 8;
+
+Vec3 orbitaMarte[] = {
+    {12, 0, 0},
+    {6, 0, 5},
+    {0, 0, 7},
+    {-6, 0, 5},
+    {-12, 0, 0},
+    {-6, 0, -5},
+    {0, 0, -7},
+    {6, 0, -5}
+};
+int nOrbitaMarte = 8;
+
+Vec3 orbitaJupiter[] = {
+    {16, 0, 0},
+    {8, 0, 7},
+    {0, 0, 10},
+    {-8, 0, 7},
+    {-16, 0, 0},
+    {-8, 0, -7},
+    {0, 0, -10},
+    {8, 0, -7}
+};
+int nOrbitaJupiter = 8;
+
+Vec3 orbitaSaturno[] = {
+    {20, 0, 0},
+    {10, 0, 8},
+    {0, 0, 12},
+    {-10, 0, 8},
+    {-20, 0, 0},
+    {-10, 0, -8},
+    {0, 0, -12},
+    {10, 0, -8}
+};
+int nOrbitaSaturno = 8;
+
 void init(void) 
 {
    glClearColor (0.0, 0.0, 0.0, 0.0);
@@ -15,8 +143,8 @@ void init(void)
 void display(void)
 {
    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-   glLoadIdentity ();             /* clear the matrix */
-   gluLookAt ( 0.0, 7.0, 15.0,  // lookFrom
+   glLoadIdentity ();             
+   gluLookAt ( 0.0, 7.0, 30.0,  // lookFrom
                0.0, 0.0, 0.0,  // lookAt
                0.0, 1.0, 0.0); // Up
 
@@ -26,21 +154,65 @@ void display(void)
       glutSolidSphere (1.0, 16, 16);
    glPopMatrix();
 
+   // Primeiro astro - Mercurio
+   Vec3 pos1 = getSplinePoint(orbitaMercurio, norbitaMercurio, t1);
+
    glPushMatrix();
-      glRotatef(angulo, 0, 1, 0);
-      glColor3f (1.0, 0.67, 0.0);
-      glTranslatef(0, 0, -3.0);
-      glScalef (0.5, 0.5, 0.5);      /* modeling transformation */ 
+      glTranslatef(pos1.x, pos1.y, pos1.z);
+      glColor3f (0.7, 0.47, 0.2);
+      glScalef (0.5, 0.5, 0.5);
       glutSolidSphere (1.0, 16, 16);
    glPopMatrix();
 
+   // Segundo astro = venus
+   Vec3 pos2 = getSplinePoint(orbitaVenus, norbitaVenus, t2);
+
    glPushMatrix();
-      glRotatef(angulo, 0, 1, 1);
+      glTranslatef(pos2.x, pos2.y, pos2.z);
       glColor3f (0.0, 1.0, 0.0);
-      glTranslatef(-5, 0, 0.0);
-      glScalef (0.5, 0.5, 0.5);      /* modeling transformation */ 
+      glScalef (0.5, 0.5, 0.5);
       glutSolidSphere (1.0, 16, 16);
    glPopMatrix();
+
+   // Terceiro astro - Terra
+   Vec3 posTerra = getSplinePoint(orbitaTerra, nOrbitaTerra, t3);
+
+   glPushMatrix();
+      glTranslatef(posTerra.x, posTerra.y, posTerra.z);
+      glColor3f(0.2, 0.5, 1.0);
+      glScalef(0.6, 0.6, 0.6);
+      glutSolidSphere(1.0, 16, 16);
+   glPopMatrix();
+
+   // Quarto astro - Marte
+   Vec3 posMarte = getSplinePoint(orbitaMarte, nOrbitaMarte, t4);
+
+   glPushMatrix();
+      glTranslatef(posMarte.x, posMarte.y, posMarte.z);
+      glColor3f(0.8, 0.3, 0.3);
+      glScalef(0.45, 0.45, 0.45);
+      glutSolidSphere(1.0, 16, 16);
+   glPopMatrix();
+
+   // Quinto astro - Jupiter
+   Vec3 posJupiter = getSplinePoint(orbitaJupiter, nOrbitaJupiter, t5);
+
+   glPushMatrix();
+      glTranslatef(posJupiter.x, posJupiter.y, posJupiter.z);
+      glColor3f(0.8, 0.5, 0.5);
+      glScalef(0.9, 0.9, 0.9);
+      glutSolidSphere(1.0, 16, 16);
+   glPopMatrix();
+
+   // Sexto astro - Saturno
+   Vec3 posSaturno = getSplinePoint(orbitaSaturno, nOrbitaSaturno, t6);
+   glPushMatrix();
+      glTranslatef(posSaturno.x, posSaturno.y, posSaturno.z);
+      glColor3f(0.6, 0.5, 0.5);
+      glScalef(0.9, 0.9, 0.9);
+      glutSolidSphere(1.0, 16, 16);
+   glPopMatrix();
+   
 
    glFlush ();
    glutSwapBuffers();
@@ -75,9 +247,21 @@ void keyboard(unsigned char key, int x, int y)
 }
 
 void update(int valor){
-    angulo += 1.0f;
-    if (angulo > 360)
-        angulo = 0;
+
+    t1 += 0.003f;
+    t2 += 0.002f;
+    t3 += 0.0015f;
+    t4 += 0.0012f;
+    t5 += 0.0008f;
+    t6 += 0.0006f;
+    t7 += 0.0004f;
+    t8 += 0.0003f;
+   float progressoTempos[8] = {t1, t2, t3, t4, t5, t6, t7, t8};
+
+    for (int i = 0 ; i < 8 ; i++){
+      if(progressoTempos[i] >= 1.0f) progressoTempos[i] = 0.0;
+    }
+
     glutPostRedisplay();
     glutTimerFunc(16, update, 0);
 }
